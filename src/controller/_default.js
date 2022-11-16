426379c556fd262ceb37475bd7dec0c3bfa67c5e
Daniel Duersteler <daniel.duersteler@wildtier.ch>
2022-11-16 11:28:23 +0100

    updated

--- src/controller/_default.js
+++ src/controller/_default.js
@@ -1,36 +1,105 @@
+/**
+ * this is an example js page controller
+ * which uses the modules
+ */
 (function ($) {
     $.fn.controller = function (options, callback) {
         var api = {};
-        var defaults = {};
+        var defaults = {
+            // ajax event callbacks
+            beforeSend: $.noop,
+            complete: $.noop,
+            error: $.noop
+        };
         var controls = {};
+        var settings = $.extend({}, defaults, options);
 
+        /**
+         * initial query to build modules
+         * from server data
+         */
         var init = function () {
             $.ajax({
                 url: window.location.href,
                 type: 'GET',
                 success: build,
-                beforeSend: function () {
-                    console.log('sending request ...');
-                },
-                complete: function () {
-                    console.log('complete');
-                },
-                error: function (xhr, status, error) {
-                    console.log('error', status, error);
-                },
+                beforeSend: settings.complete,
+                complete: settings.complete,
+                error: settings.error,
             });
         };
 
-        // build the markup and events for all controls
+        /**
+         * build modules with server data
+         * @param {Object} data 
+         */
         var build = function (data) {
+            // on change callback for all modules
+            var onChange = query;
             $.each(data.controls ?? {}, function (k, v) {
                 // create the module instance, if module is found
                 if ($.isFunction($.fn[v.ctrltype])) {
                     controls[k] = $.fn[v.ctrltype](
-                        $.extend({}, { id: k + '_1', ctrlelem: $('#' + k), onChange: $.noop }, v)
+                        $.extend({}, { id: k + '_1', ctrlelem: $('#' + k), onChange: onChange }, v)
                     );
                 }
             });
+        };
+
+        /**
+         * update modules with server data
+         * @param {Object} data 
+         */
+        var update = function (data) {
+            $.each(data.controls, function (k, v) {
+                controls[k].update(v);
+            });
+        };
+
+        /**
+         * query the server with parameters
+         * from the modules
+         */
+        var query = function () {
+            $.ajax({
+                url: window.location.href,
+                type: 'GET',
+                data: createState(),
+                success: update,
+                beforeSend: settings.beforeSend,
+                complete: settings.complete,
+                error: settings.error,
+            });
+        };
+
+        /**
+         * create state and array of module 
+         * values to query the server
+         * @param {Bool} replace 
+         * @returns {Array} query parameters
+         */
+        var createState = function (replace) {
+            var p = {};
+            $.each(controls, function (k, v) {
+                if ($.isFunction(v.ctrlname) && $.isFunction(v.value))
+                    p[v.ctrlname()] = v.value();
+            });
+            // create querystring from control values
+            var q = $.param(p);
+            if (replace === true) {
+                window.history.replaceState(
+                    { url: window.location.href.split('?')[0] + '?' + q },
+                    window.location.href.split('?')[0] + '?' + q,
+                    window.location.href.split('?')[0] + '?' + q
+                );
+            } else {
+                window.history.pushState(
+                    { url: window.location.href.split('?')[0] + '?' + q },
+                    window.location.href.split('?')[0] + '?' + q,
+                    window.location.href.split('?')[0] + '?' + q
+                );
+            }
+            return p;
         };
 
         init();
---
+++ src/utility/history.js
@@ -0,0 +1,25 @@
+/**
+ * monkey patches for 
+ * onpushstate and onreplacestate events
+ */
+(function (history) {
+    // backup original
+    var pushState = history.pushState;
+    var replaceState = history.replaceState;
+    // add pushState monkey patch
+    history.pushState = function (state) {
+        // if user function was added
+        if (typeof history.onpushstate == "function")
+            history.onpushstate({ state: state });
+        // original functionality
+        return pushState.apply(history, arguments);
+    };
+    // add replaceState monkey patch
+    history.replaceState = function (state) {
+        // if user function was added
+        if (typeof history.onreplacestate == "function")
+            history.onreplacestate({ state: state });
+        // original functionality
+        return replaceState.apply(history, arguments);
+    };
+})(window.history);
