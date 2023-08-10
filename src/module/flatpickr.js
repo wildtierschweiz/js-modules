e83679eb3d223d981e525d5a5d00f4e14b0738d3
Daniel Duersteler <daniel.duersteler@wildtier.ch>
2023-08-10 22:39:04 +0200

    added flatpickr

---
+++ src/module/flatpickr.js
@@ -0,0 +1,47 @@
+(function ($) {
+    $.fn.flatpickr = function (options) {
+        var api = {};
+        var defaults = {
+            ctrlelem: null,
+            ctrltype: '',
+            ctrldata: {
+                dateFormat: "Y-m-d",
+                enableTime: false,
+                enableSeconds: false,
+                time_24hr: true,
+                maxDate: null,
+                minDate: null,
+                defaultDate: new Date(),
+                locale: 'en'
+            },
+            disabled: false,
+            readonly: false,
+            visible: true,
+        };
+        var settings = $.extend(true, {}, defaults, options);
+
+        var init = function () {
+            build();
+        };
+
+        var create = function () {
+            $e = $('<input></input>');
+            $e.flatpickr(settings.ctrlelem);
+            return $e;
+        }
+
+        var build = function () {
+            settings.ctrlelem.empty();
+            if (settings.visible === true)
+                settings.ctrlelem.append(create());
+        };
+
+        api.update = function (data) {
+            settings = $.extend({}, settings, data);
+            build();
+        };
+
+        init();
+        return api;
+    };
+})($);
